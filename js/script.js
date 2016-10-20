5355e215dec1f2e815240083c61414aa915441f0
Gleidson <gleidsondeveloper@gmail.com>
2016-10-20 15:01:55 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
 		closeOnClick: true
 	});
 	aMenu();
+	pBanner();
 })
 
 $(window).resize(function(){})
@@ -12,4 +13,24 @@ function aMenu(){
 		$("#aMenu li").removeClass("active");
 		$(this).addClass("active");
 	})
+}
+function pBanner(){
+	var a = $(window).width();
+
+	if(a > 1024){
+		$(".pBanner").jCarouselLite({
+			btnNext: ".pBanner .next",
+			btnPrev: ".pBanner .prev",
+			speed: 500,
+			visible: 2,
+			start: 0.58
+		});
+	}else{
+		$(".pBanner").jCarouselLite({
+			btnNext: ".pBanner .next",
+			btnPrev: ".pBanner .prev",
+			speed: 500,
+			visible: 1,
+		});
+	}
 }
